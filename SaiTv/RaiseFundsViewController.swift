eaf49d8738c038af2563ed7f9e977fab711ca0ce
Harsha <harshavardhan819@gmail.com>
2020-09-14 13:15:15 +0530

    Inital Commit

---
+++ SaiTv/RaiseFundsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RaiseFundsViewController.swift
+//  SaiTv
+//
+//  Created by Sarveshwar Reddy on 16/04/20.
+//  Copyright © 2020 Sarveshwar Reddy. All rights reserved.
+//
+
+import UIKit
+
+class RaiseFundsViewController: UIViewController {
+
+    @IBOutlet weak var fundsLabel: UILabel!
+    @IBOutlet weak var sixLabel: UILabel!
+    @IBOutlet weak var oneLabel: UILabel!
+    @IBOutlet weak var fiveLabel: UILabel!
+    @IBOutlet weak var threeLabel: UILabel!
+    @IBOutlet weak var twoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()    }
+
+}
